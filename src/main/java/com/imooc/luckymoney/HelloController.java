5beb1124c9396b298a2ac00854cc79a0578f27b0
Dapeng Jin <j451849359@gmail.com>
2022-02-25 23:18:01 -0800

    1. 配置 的书写与使用：     设置端口，path     设置变量         在配置里使用配置 ${}         在程序里使用配置 @Value("${}")     设置对象         注入对象到配置文件 @Component + @ConfigurationProperties(prefix="limit")         在配置里给对象赋值         在程序里使用 @Autowired
    2. 在不同环境下，使用不同的配置 development or production
    
    3. 部署时指定配置 java -jar -Dspring.profiles.active=prod xxxx.jar

--- src/main/java/com/imooc/luckymoney/HelloController.java
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -1,12 +1,29 @@
 package com.imooc.luckymoney;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 public class HelloController {
+
+    @Value("${minMoney}")
+    private BigDecimal minMoney;
+
+    @Value("${description}")
+    private String description;
+
+    @Autowired
+    private LimitConfig limitconfig;
+
     @GetMapping("/hello")
     public String say(){
-        return "Hello World";
+        return "Hello World" + "<br />\n" +
+                "minMoney: " + minMoney + "<br />\n" +
+                "说明：" + description + "<br />\n" +
+                "LimitConfig Description: " + limitconfig.getDescription();
     }
 }
